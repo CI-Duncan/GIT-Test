bb040addded5a0150e3173613613896c35216550
Hannah Eves <heves.work@outlook.com>
2024-06-26 11:01:47 +0000

    Added members

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let formElement = document.getElementById("myForm");
+//     formElement.addEventListener('submit', displayName);
 
-document.addEventListener("DOMContentLoaded", function () {
-    let formElement = document.getElementById("myForm");
-    formElement.addEventListener('submit', displayName);
+//     function displayName(e) {
+//         e.preventDefault();
+//         const results = document.getElementById("fullName")
+//         let text = results.value;
+//         document.getElementById("display-message").innerText = text;
 
-    function displayName(e) {
-        e.preventDefault();
-        const results = document.getElementById("fullName")
-            let text = results.value;
-            document.getElementById("display-message").innerText = text;
-        
-    };
-});
+//     };
+// });
 
 function pullReq() {
     console.log("Hello, I'm creating a pull request");
@@ -18,3 +17,30 @@ function pullReq() {
 
 pullReq();
 
+const groupMembers = 6
+
+let group = {
+    members: [{
+            Name: "Duncan"
+        },
+        {
+            Name: "Craig"
+        },
+        {
+            Name: "Chelsea"
+        },
+        {
+            Name: "Ahmed"
+        },
+        {
+            Name: "Henry"
+        },
+        {
+            Name: "Lily"
+        },
+    ]
+};
+
+for (let member of group.members) {
+    console.log(member.Name)
+}
